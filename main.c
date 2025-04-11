f6e83ca19aa8f8e18c8b1ee2c87eebbbc5948aa4
Thomastlf <ttaillefer12@gmail.com>
2025-04-11 16:08:53 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <version1.h>
+#define NOMBRE_COMBATTANT
+
+
+typedef struct{
+  Combattant combattant1;
+  Combattant combattant2;
+  Combattant combattant3;
+}Equipe;
+
+void degat(Combattant combattant,int degat){
+  combattant.pv=combattant.pv-degat;
+}
+
+void soin(Combattant combattant, int soin){
+  combattant.pv=combattant.pv+soin;
+}
+
+Combattant* creerlistecombattant()
+  
+
+void construireEquipe(Equipe* equipe1,Equipe* equipe2,Combattant tab[]){
+  printf("Les combattants disponibles sont :\n");
+for (int i=0;i<NOMBRE_COMBATTANT,i++){
+  printf("tab[i].nom"\n);}
+  int tab[6];
+  
+  for (int j=0;j<6;j++){
+    switch (i){
+      case 0:
+        
+  
+    
+  
+}
+  
+
+int main(){
+int tour=1;
+int a;
+while(zeus.pv>0 && hades.pv>0){
+  if (tour%2==0){
+    scanf("%d",&a);
+    if(a==1)}
+      degat(hades,CompeteneceZeus.valeur);}
+    }
+  else{
+    scanf("%sd";&a);
+    if(a==1){
+      degat(zeus,Competencehades.valeur);}}
+  
+  tour=tour+1;
+  return 0;
+}
