5a88256ca66bf1bd2438e4c9b0db1a916e7cf937
Thomastlf <ttaillefer12@gmail.com>
2025-04-11 14:10:16 +0200

    Update version1.c

--- version1.c
+++ version1.c
@@ -1,1 +1,17 @@
+#include <stdlib.h>
 
+typedef struct{
+  int pv;
+  int pvmax;
+  int attaque;
+  int defense;
+  int agilite;
+  int vitesse;
+}  Combattant;
+
+
+  
+
+int main(){
+  return 0;
+}
