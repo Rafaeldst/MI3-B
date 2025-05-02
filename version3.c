2433e9483bfb43dab49a67f409b6033b56e1cc94
Rafaeldst <rafael.doussaint@gmail.com>
2025-05-02 14:10:42 +0200

    Update version3.c

--- version3.c
+++ version3.c
@@ -82,6 +82,15 @@ void acceleration(Combattant* combattant){
   combattant->vitesse=combattant->vitesse+30;
 }
 
+void benediction_divine(Combattant* combattant){
+  Effet effet;
+  strcpy(effet.nom, "benediction_divine");
+  effet.tour_restant=3;
+  combattant->effet_special[nbr_effet_actif]=effet;
+  combattant->nbr_effet_actif++;
+  combattant->pv=combattant->pv+10;
+}
+
 void appliquer_technique(Combattant* cible,Competence competence,Combattant* lanceur){//fonction qui va faire des if pour trouver si la technique est un degat,un soin, un endormissement... et applique la technique en conséquence
   printf("%s utilise %s sur %s !\n",lanceur->nom;cible->lanceur);
 
