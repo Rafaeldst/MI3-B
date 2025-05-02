53151efd23591735d5183ae8bf4e2f7de69b6a1c
Ibrahim75cy <ibrahim.fridjine@gmail.com>
2025-05-02 15:53:39 +0200

    Update version3.c

--- version3.c
+++ version3.c
@@ -138,43 +138,6 @@ void appliquer_technique(Competence* competence,Combattant* lanceur, Combattant 
   }
 }
 
-void maj_effet(Combattant* combattant) {
-    for (int i = 0; i < combattant->nbr_effet_actif; i++) {
-        combattant->effet_special[i].tour_restant--;
-        if (combattant->effet_special[i]==0){
-            retirer_effet(combattant);
-        }
-    }
-}
-
-void retirer_effet(Combattant* combattant){
-    combattant->nbr_effet_actif--;
-    
-}
-
-void maj_recharge(Combattant* combattant){
-    for (int i = 0; i < 4; i++) {
-        if (combattant->competence[i].tour_recharge > 0) {
-            combattant->competence[i].tour_recharge--;
-        }
-    }
-}
-
-void maj_tous_stats(Combattant* combattant){
-    maj_recharge(combattant);
-    maj_vitesse(combattant);
-    maj_effet(combattant);
-}
-    
-    void maj_tous(Combattant Equipe1[],Combattant Equipe2[]){
-    for (int i=0;i<3;i++){
-        if vivant(Equipe2[i]){
-            maj_tous_stats(Equipe2[i]);
-        }
-        if vivant(Equipe2[i]){
-            maj_tous_stats(Equipe2[i]);
-        }    
-}
 
 Combattant charger_combattant(const char* fichier_combattant){ //fonction qui charge les données d'un combattant contenues dans un fichier
   Combattant c;
@@ -269,6 +232,52 @@ printf("\n");
 
 void maj_vitesse(Combattant* combattant){
   combattant->barre_action=combattant->barre_action+combattant->vitesse;}
+
+void maj_effet(Combattant* combattant) {
+    for (int i = 0; i < combattant->nbr_effet_actif; i++) {
+        combattant->effet_special[i].tour_restant--;
+        if (combattant->effet_special[i]==0){
+          retirer_stats(combattant,i);  
+          retirer_effet(combattant,i);
+            i--;
+        }
+    }
+}
+
+void retirer_stats(Combattant* combattant,int index){
+
+}
+
+void retirer_effet(Combattant* combattant,int index){
+  for (; index < combattant->nbr_effet_actif - 1; index++){
+        combattant->effet_special[index] = combattant->effet_special[index+ 1];
+    }
+    combattant->nbr_effet_actif--;
+}
+
+void maj_recharge(Combattant* combattant){
+    for (int i = 0; i < 4; i++) {
+        if (combattant->competence[i].tour_recharge > 0) {
+            combattant->competence[i].tour_recharge--;
+        }
+    }
+}
+
+void maj_tous_stats(Combattant* combattant){
+    maj_recharge(combattant);
+    maj_vitesse(combattant);
+    maj_effet(combattant);
+}
+    
+    void maj_tous(Combattant Equipe1[],Combattant Equipe2[]){
+    for (int i=0;i<3;i++){
+        if vivant(Equipe2[i]){
+            maj_tous_stats(Equipe2[i]);
+        }
+        if vivant(Equipe2[i]){
+            maj_tous_stats(Equipe2[i]);
+        }    
+}
 
 int charge(Combattant combattant){
   if (combattant.barre_action<99){
