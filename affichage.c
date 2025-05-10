a81e7efca525a22f3e8ed9a112730bb83c8a7977
Thomastlf <ttaillefer12@gmail.com>
2025-05-10 14:59:00 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -1,1 +1,373 @@
+void affiche_tous_perso(Combattant* Equipe1[],Combattant* Equipe2[],char* equipe1_Nom,char* equipe2_Nom,Combattant* attaquant){
+  int a;
+  printf("\n");
+  
 
+
+    printf("@[%s]",equipe1_Nom);//1
+    for (int i=0;i<TAILLE_AFFICHAGE-3-strlen(equipe1_Nom);i++){
+      printf("@");
+    }
+    printf("\n@");
+
+    for (int i=0;i<TAILLE_AFFICHAGE-2;i++){//2
+      printf(" ");
+    }
+    printf("@\n@");
+
+    a=TAILLE_AFFICHAGE-1;//3
+    for (int i=0;i<3;i++){
+      printf(" ");
+      a--;
+    }
+    printf("%s [1]",Equipe1[0]->nom);
+    a=a-4-strlen(Equipe1[0]->nom);
+    for(int i=0;i<33-4-strlen(Equipe1[0]->nom);i++){
+      printf(" ");a--;
+    }
+    printf("%s [2]",Equipe1[1]->nom);
+    a=a-4-strlen(Equipe1[1]->nom);
+    for(int i=0;i<33-4-strlen(Equipe1[1]->nom);i++){
+      printf(" ");a--;
+    }
+    printf("%s [3]",Equipe1[2]->nom);
+    a=a-4-strlen(Equipe1[2]->nom);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+    printf("@\n@");
+
+    a=TAILLE_AFFICHAGE-1;//4
+    for (int i=0;i<3;i++){
+      printf(" ");
+      a--;
+    }
+    printf("%d/%d",Equipe1[0]->pv,Equipe1[0]->pvmax);
+    a=a-1-compter_chiffre(Equipe1[0]->pv)-compter_chiffre(Equipe1[0]->pvmax);
+    for(int i=0;i<33-1-compter_chiffre(Equipe1[0]->pv)-compter_chiffre(Equipe1[0]->pvmax);i++){
+      printf(" ");a--;
+    }
+    printf("%d/%d",Equipe1[1]->pv,Equipe1[1]->pvmax);
+    a=a-1-compter_chiffre(Equipe1[1]->pv)-compter_chiffre(Equipe1[1]->pvmax);
+    for(int i=0;i<33-1-compter_chiffre(Equipe1[1]->pv)-compter_chiffre(Equipe1[1]->pvmax);i++){
+      printf(" ");a--;
+    }
+    printf("%d/%d",Equipe1[2]->pv,Equipe1[2]->pvmax);
+    a=a-1-compter_chiffre(Equipe1[2]->pv)-compter_chiffre(Equipe1[2]->pvmax);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+    printf("@\n@");
+
+    a=TAILLE_AFFICHAGE-1;//5
+    for (int i=0;i<3;i++){
+      printf(" ");
+      a--;
+    }
+    printf("%d/100",Equipe1[0]->barre_action);
+    a=a-1-compter_chiffre(Equipe1[0]->barre_action)-3;
+    for(int i=0;i<33-1-compter_chiffre(Equipe1[0]->barre_action)-3;i++){
+      printf(" ");a--;
+    }
+    printf("%d/100",Equipe1[1]->barre_action);
+    a=a-1-compter_chiffre(Equipe1[1]->barre_action)-3;
+    for(int i=0;i<33-1-compter_chiffre(Equipe1[1]->barre_action)-3;i++){
+      printf(" ");a--;
+    }
+    printf("%d/100",Equipe1[2]->barre_action);
+    a=a-1-compter_chiffre(Equipe1[2]->barre_action)-3;
+    for(int i=0;i<a-1;i++){
+      printf(" ");
+    }
+    printf("@\n@");
+
+    a=TAILLE_AFFICHAGE-1;//6
+    for (int i=0;i<3;i++){
+      printf(" ");
+      a--;
+    }
+    afficher_effet(Equipe1[0]);
+    a=a-Equipe1[0]->nbr_effet_actif*2;
+    for(int i=0;i<33-(Equipe1[0]->nbr_effet_actif*2);i++){
+      printf(" ");
+      a--;
+    }
+    afficher_effet(Equipe1[1]);
+    a=a-Equipe1[1]->nbr_effet_actif*2;
+    for(int i=0;i<33-(Equipe1[1]->nbr_effet_actif*2);i++){
+      printf(" ");
+      a--;
+    }
+    afficher_effet(Equipe1[2]);
+    a=a-(Equipe1[2]->nbr_effet_actif*2);
+    for(int i=0;i<a-1;i++){
+      printf(" ");
+    }
+    printf("@\n@");
+
+    for (int i=0;i<TAILLE_AFFICHAGE-2;i++){//7
+      printf(" ");
+    }
+    printf("@\n");
+
+    for (int i=0;i<TAILLE_AFFICHAGE;i++){//8
+      printf("@");
+    }
+    printf("\n\n");
+
+
+
+
+
+
+  printf("@[%s]",equipe2_Nom);//1
+  for (int i=0;i<TAILLE_AFFICHAGE-3-strlen(equipe2_Nom);i++){
+    printf("@");
+  }
+  printf("\n@");
+
+  for (int i=0;i<TAILLE_AFFICHAGE-2;i++){//2
+    printf(" ");
+  }
+  printf("@\n@");
+
+  a=TAILLE_AFFICHAGE-1;//3
+  for (int i=0;i<3;i++){
+    printf(" ");
+    a--;
+  }
+  printf("%s [1]",Equipe2[0]->nom);
+  a=a-4-strlen(Equipe2[0]->nom);
+  for(int i=0;i<33-4-strlen(Equipe2[0]->nom);i++){
+    printf(" ");a--;
+  }
+  printf("%s [2]",Equipe2[1]->nom);
+  a=a-4-strlen(Equipe2[1]->nom);
+  for(int i=0;i<33-4-strlen(Equipe2[1]->nom);i++){
+    printf(" ");a--;
+  }
+  printf("%s [3]",Equipe2[2]->nom);
+  a=a-4-strlen(Equipe2[2]->nom);
+  for (int i=0;i<a-1;i++){
+    printf(" ");
+  }
+  printf("@\n@");
+
+  a=TAILLE_AFFICHAGE-1;//4
+  for (int i=0;i<3;i++){
+    printf(" ");
+    a--;
+  }
+  printf("%d/%d",Equipe2[0]->pv,Equipe2[0]->pvmax);
+  a=a-1-compter_chiffre(Equipe2[0]->pv)-compter_chiffre(Equipe2[0]->pvmax);
+  for(int i=0;i<33-1-compter_chiffre(Equipe2[0]->pv)-compter_chiffre(Equipe2[0]->pvmax);i++){
+    printf(" ");a--;
+  }
+  printf("%d/%d",Equipe2[1]->pv,Equipe2[1]->pvmax);
+  a=a-1-compter_chiffre(Equipe2[1]->pv)-compter_chiffre(Equipe2[1]->pvmax);
+  for(int i=0;i<33-1-compter_chiffre(Equipe2[1]->pv)-compter_chiffre(Equipe2[1]->pvmax);i++){
+    printf(" ");a--;
+  }
+  printf("%d/%d",Equipe2[2]->pv,Equipe2[2]->pvmax);
+  a=a-1-compter_chiffre(Equipe2[2]->pv)-compter_chiffre(Equipe2[2]->pvmax);
+  for (int i=0;i<a-1;i++){
+    printf(" ");
+  }
+  printf("@\n@");
+
+  a=TAILLE_AFFICHAGE-1;//5
+  for (int i=0;i<3;i++){
+    printf(" ");
+    a--;
+  }
+  printf("%d/100",Equipe2[0]->barre_action);
+  a=a-1-compter_chiffre(Equipe2[0]->barre_action)-3;
+  for(int i=0;i<33-1-compter_chiffre(Equipe2[0]->barre_action)-3;i++){
+    printf(" ");a--;
+  }
+  printf("%d/100",Equipe2[1]->barre_action);
+  a=a-1-compter_chiffre(Equipe2[1]->barre_action)-3;
+  for(int i=0;i<33-1-compter_chiffre(Equipe2[1]->barre_action)-3;i++){
+    printf(" ");a--;
+  }
+  printf("%d/100",Equipe2[2]->barre_action);
+  a=a-1-compter_chiffre(Equipe2[2]->barre_action)-3;
+  for(int i=0;i<a-1;i++){
+    printf(" ");
+  }
+  printf("@\n@");
+
+  a=TAILLE_AFFICHAGE-1;//6
+  for (int i=0;i<3;i++){
+    printf(" ");
+    a--;
+  }
+  afficher_effet(Equipe2[0]);
+  a=a-Equipe2[0]->nbr_effet_actif*2;
+  for(int i=0;i<33-(Equipe2[0]->nbr_effet_actif*2);i++){
+    printf(" ");
+    a--;
+  }
+  afficher_effet(Equipe2[1]);
+  a=a-Equipe2[1]->nbr_effet_actif*2;
+  for(int i=0;i<33-(Equipe2[1]->nbr_effet_actif*2);i++){
+    printf(" ");
+    a--;
+  }
+  afficher_effet(Equipe2[2]);
+  a=a-(Equipe2[2]->nbr_effet_actif*2);
+  for(int i=0;i<a-1;i++){
+    printf(" ");
+  }
+  printf("@\n@");
+
+  for (int i=0;i<TAILLE_AFFICHAGE-2;i++){//7
+    printf(" ");
+  }
+  printf("@\n");
+
+  for (int i=0;i<TAILLE_AFFICHAGE;i++){//8
+    printf("@");
+  }
+  printf("\n");
+  
+
+
+
+    a=TAILLE_AFFICHAGE-1;//8
+    for (int i=0;i<3;i++){
+      printf(" ");
+      a--;
+    }
+  printf("\n");
+
+    printf("-----------------%s--------------------\n",attaquant->nom);
+    a=a-strlen(attaquant->nom);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+
+    printf("\n");
+
+
+
+    a=TAILLE_AFFICHAGE-1;//9
+    for (int i=0;i<5;i++){
+      printf(" ");
+      a--;
+    }
+    printf("[1] ");
+    a=a-4;
+    printf("%s",attaquant->competence[0]->nom);
+    a=a-strlen(attaquant->competence[0]->nom);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+
+    printf("\n");
+    a=TAILLE_AFFICHAGE-1;//10
+    for (int i=0;i<7;i++){
+      printf(" ");
+      a--;
+    }
+    printf("%s",attaquant->competence[0]->description);
+        if(attaquant->competence[0]->tour_recharge_restant != 0){
+    for (int i=0 ; i<9;i++){
+      printf(" ");
+    }
+    printf("[tour de recharge : %d]", attaquant->competence[0]->tour_recharge_restant);}
+    a=a-strlen(attaquant->competence[0]->description);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+    printf("\n");
+
+    a=TAILLE_AFFICHAGE-1;//11
+    for (int i=0;i<5;i++){
+      printf(" ");
+      a--;
+    }
+    printf("[2] ");
+    a=a-4;
+    printf("%s",attaquant->competence[1]->nom);
+        if(attaquant->competence[1]->tour_recharge_restant != 0){
+    for (int i=0 ; i<9;i++){
+      printf(" ");
+    }
+    printf("[tour de recharge : %d]", attaquant->competence[1]->tour_recharge_restant);}
+    a=a-strlen(attaquant->competence[1]->nom);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+
+    printf("\n");
+    a=TAILLE_AFFICHAGE-1;//12
+    for (int i=0;i<7;i++){
+      printf(" ");
+      a--;
+    }
+    printf("%s",attaquant->competence[1]->description);
+    a=a-strlen(attaquant->competence[1]->description);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+    printf("\n");
+
+    a=TAILLE_AFFICHAGE-1;//13
+    for (int i=0;i<5;i++){
+      printf(" ");
+      a--;
+    }
+    printf("[3] ");
+    a=a-4;
+    printf("%s",attaquant->competence[2]->nom);
+    if(attaquant->competence[2]->tour_recharge_restant != 0){
+    for (int i=0 ; i<9;i++){
+      printf(" ");
+    }
+    printf("[tour de recharge : %d]", attaquant->competence[2]->tour_recharge_restant);}
+    a=a-strlen(attaquant->competence[2]->nom);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+
+    printf("\n");
+    a=TAILLE_AFFICHAGE-1;//14
+    for (int i=0;i<7;i++){
+      printf(" ");
+      a--;
+    }
+    printf("%s",attaquant->competence[2]->description);
+    a=a-strlen(attaquant->competence[2]->description);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+    printf("\n");
+
+    a=TAILLE_AFFICHAGE-1;//15
+    for (int i=0;i<5;i++){
+      printf(" ");
+      a--;
+    }
+    printf("[4] ");
+    a=a-4;
+    printf("%s",attaquant->competence[3]->nom);
+        if(attaquant->competence[3]->tour_recharge_restant != 0){
+    for (int i=0 ; i<9;i++){
+      printf(" ");
+    }
+    printf("[tour de recharge : %d]", attaquant->competence[3]->tour_recharge_restant);}
+    a=a-strlen(attaquant->competence[3]->nom);
+    for (int i=0;i<a-1;i++){
+      printf(" ");
+    }
+
+    printf("\n");
+    a=TAILLE_AFFICHAGE-1;//16
+    for (int i=0;i<7;i++){
+      printf(" ");
+      a--;
+    }
+    printf("%s",attaquant->competence[3]->description);
+    a=a-strlen(attaquant->competence[3]->description);
+}
