22faebe45a2f886e5d9aace645c11026a3ce55bc
Thomastlf <ttaillefer12@gmail.com>
2025-05-10 17:02:36 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,10 +1,19 @@
-#inclure "projet.h"
+#ifndef COMBAT_H
+#define COMBAT_H
+
+
+#include "projet.h"
 
 int meilleur_scan();
-void verifier_erreur_fichier(FILE* fichier)
+int vivant(Combattant* c);
+void soin_recharge(Combattant* c, int valeur, Competence* competence);
+void degat_recharge(Combattant* c, int valeur, Competence* competence);
+void acceleration(Combattant* c, Competence* competence);
+void protection(Combattant* c, Competence* competence);
+void verifier_erreur_fichier(FILE* fichier);
 Combattant* choisir_cible(Combattant* lanceur,Combattant* equipe1[], Combattant* equipe2[], Competence* competence,int mode, int difficulte);
 void appliquer_technique(Competence* competence,Combattant* lanceur, Combattant* equipe1[], Combattant* equipe2[], int mode, int difficulte);
-charger_competence(char* fichier_competence);
+Competence *charger_competence(char* fichier_competence);
 Combattant* charger_combattant(char* fichier_combattant);
 void creer_equipe(Combattant* equipe1[],Combattant* equipe2[]);
 void creer_equipeIA(Combattant* equipe1[],Combattant* equipe2[]);
@@ -13,3 +22,5 @@ Competence* choisir_attaqueIAfaiblemoyen(Combattant* combattant);
 Competence* choisir_attaqueIAfort(Combattant* combattant);
 int compter_chiffre(int n);
 Combattant* plus_rapide(Combattant* equipe1[],Combattant* equipe2[]);
+
+#endif
