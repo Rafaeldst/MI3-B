a34674c664eecd23bd71c3c371e996ad973004c9
Thomastlf <ttaillefer12@gmail.com>
2025-05-10 17:03:09 +0200

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -1,5 +1,10 @@
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+
 #include "projet.h"
 
 void afficher_personnages_disponibles(int deja_choisi[], char personnages_disponibles[NOMBRE_PERSO][TAILLE_NOM_PERSO]);
 void affiche_tous_perso(Combattant* Equipe1[], Combattant* Equipe2[], char* equipe1_Nom, char* equipe2_Nom, Combattant* attaquant);
 void afficher_effet(Combattant* combattant);
+
+#endif
