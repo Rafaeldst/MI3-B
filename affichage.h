90d1c4d27b8e1442b02d087d374c18ad50983b7e
Thomastlf <ttaillefer12@gmail.com>
2025-05-10 15:49:27 +0200

    Create affichage.h

---
+++ affichage.h
@@ -0,0 +1,5 @@
+#include "projet.h"
+
+void afficher_personnages_disponibles(int deja_choisi[], char personnages_disponibles[NOMBRE_PERSO][TAILLE_NOM_PERSO]);
+void affiche_tous_perso(Combattant* Equipe1[], Combattant* Equipe2[], char* equipe1_Nom, char* equipe2_Nom, Combattant* attaquant);
+void afficher_effet(Combattant* combattant);
