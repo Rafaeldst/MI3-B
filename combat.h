96e6225467beb90218d55c8d26a593326076a9cc
Thomastlf <ttaillefer12@gmail.com>
2025-05-10 16:03:07 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,1 +1,15 @@
+#inclure "projet.h"
 
+int meilleur_scan();
+void verifier_erreur_fichier(FILE* fichier)
+Combattant* choisir_cible(Combattant* lanceur,Combattant* equipe1[], Combattant* equipe2[], Competence* competence,int mode, int difficulte);
+void appliquer_technique(Competence* competence,Combattant* lanceur, Combattant* equipe1[], Combattant* equipe2[], int mode, int difficulte);
+charger_competence(char* fichier_competence);
+Combattant* charger_combattant(char* fichier_combattant);
+void creer_equipe(Combattant* equipe1[],Combattant* equipe2[]);
+void creer_equipeIA(Combattant* equipe1[],Combattant* equipe2[]);
+Competence* choisir_attaque(Combattant* combattant);
+Competence* choisir_attaqueIAfaiblemoyen(Combattant* combattant);
+Competence* choisir_attaqueIAfort(Combattant* combattant);
+int compter_chiffre(int n);
+Combattant* plus_rapide(Combattant* equipe1[],Combattant* equipe2[]);
